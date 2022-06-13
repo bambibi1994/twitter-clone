7f3baf8a6dd094325703fe3fefa60c3ad7abe796
unknown <Bambibi_1994@yahoo.com.vn>
2022-06-13 12:30:40 -0500

    submit at end of day2

--- TechTalentTwitter/src/main/java/com/tts/TechTalentTwitter/TechTalentTwitterApplication.java
+++ TechTalentTwitter/src/main/java/com/tts/TechTalentTwitter/TechTalentTwitterApplication.java
@@ -10,6 +10,5 @@ public class TechTalentTwitterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TechTalentTwitterApplication.class, args);
-		System.out.println("Hello World!");
 	}
 }
